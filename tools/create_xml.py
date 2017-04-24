31920b8ed88c6cd30d4c37f1580d0744c7187cf9
xinleipan <xinleipan@berkeley.edu>
2017-04-24 13:14:24 -0700

    update a more clever design of create_xml.py

--- tools/create_xml.py
+++ tools/create_xml.py
@@ -10,17 +10,11 @@ def prettify(elem):
     reparsed = minidom.parseString(rough_string)
     return reparsed.toprettyxml(indent="  ")
 
-# CLASSES = ('__background__', 'person', 'rider', 'car',
-#            'truck', 'bus', 'motorcycle', 'bicycle', 
-#                         'caravan', 'trailer', 'fence', 'guard rail',
-#                         'tunnel', 'traffic sign', 'traffic light')
-# CLASSES = ('__background__')
-
 class object_cls:
-	def __init__(self, classId, bbox, truncated):
-		self.classId = classId
-		self.bbox = bbox
-        	self.truncated = truncated
+    def __init__(self, classId, bbox, truncated):
+        self.classId = classId
+        self.bbox = bbox
+        self.truncated = truncated
 
 def create_xml(filename, outname, im_size, CLASSES, objects=None, folder='VOC2007', database = 'The VOC2007 Database', annotation = 'PASCAL VOC2007'):
     """Create the xml file"""
@@ -30,7 +24,6 @@ def create_xml(filename, outname, im_size, CLASSES, objects=None, folder='VOC200
     
     child_filename = SubElement(top, 'filename')
     child_filename.text = filename
-    
     
     child_source = SubElement(top, 'source')
     child_source_database = SubElement(child_source, 'database')
@@ -60,244 +53,37 @@ def create_xml(filename, outname, im_size, CLASSES, objects=None, folder='VOC200
     child_segmented = SubElement(top, 'segmented')
     child_segmented.text = '0'
     
+    child_object = {}
+    child_object_name = {}
+    child_object_pose = {}
+    child_object_truncated = {}
+    child_object_difficult = {}
+    child_object_bndbox = {}
+    child_object_bndbox_xmin = {}
+    child_object_bndbox_ymin = {}
+    child_object_bndbox_xmax = {}
+    child_object_bndbox_ymax = {}
+
     for i in range(len(objects)):
-	if i == 0:
-		# print 'add one object'
-		child_object0 = SubElement(top, 'object')
-    		child_object0_name = SubElement(child_object0, 'name')
-    		child_object0_name.text = CLASSES[objects[i].classId].lower()
- 		child_object0_pose = SubElement(child_object0, 'pose')
-		child_object0_pose.text = 'Left'
-		child_object0_truncated = SubElement(child_object0, 'truncated')
-		child_object0_truncated.text = str(objects[i].truncated)
-		child_object0_difficult = SubElement(child_object0, 'difficult')
-		child_object0_difficult.text = '0'
+        child_object[i] = SubElement(top, 'object')
+        child_object_name[i] = SubElement(child_object[i], 'name')
+        child_object_name[i].text = CLASSES[objects[i].classId].lower()
+        child_object_pose[i] = SubElement(child_object[i], 'pose')
+        child_object_pose[i].text = 'Left'
+        child_object_truncated[i] = SubElement(child_object[i], 'truncated')
+        child_object_truncated[i].text = str(objects[i].truncated)
+        child_object_difficult[i] = SubElement(child_object[i], 'difficult')
+        child_object_difficult[i].text = '0'
 
-		child_object0_bndbox = SubElement(child_object0, 'bndbox')
-		child_object0_bndbox_xmin = SubElement(child_object0_bndbox, 'xmin')
-		child_object0_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object0_bndbox_ymin = SubElement(child_object0_bndbox, 'ymin')
-		child_object0_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object0_bndbox_xmax = SubElement(child_object0_bndbox, 'xmax')
-		child_object0_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object0_bndbox_ymax = SubElement(child_object0_bndbox, 'ymax')
-		child_object0_bndbox_ymax.text = str(objects[i].bbox[3])
-
-	elif i == 1:
-		# print 'add one object'
-		child_object1 = SubElement(top, 'object')
-    		child_object1_name = SubElement(child_object1, 'name')
-    		child_object1_name.text = CLASSES[objects[i].classId].lower()
- 		child_object1_pose = SubElement(child_object1, 'pose')
-		child_object1_pose.text = 'Left'
-		child_object1_truncated = SubElement(child_object1, 'truncated')
-		child_object1_truncated.text = str(objects[i].truncated)
-		child_object1_difficult = SubElement(child_object1, 'difficult')
-		child_object1_difficult.text = '0'
-
-		child_object1_bndbox = SubElement(child_object1, 'bndbox')
-		child_object1_bndbox_xmin = SubElement(child_object1_bndbox, 'xmin')
-		child_object1_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object1_bndbox_ymin = SubElement(child_object1_bndbox, 'ymin')
-		child_object1_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object1_bndbox_xmax = SubElement(child_object1_bndbox, 'xmax')
-		child_object1_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object1_bndbox_ymax = SubElement(child_object1_bndbox, 'ymax')
-		child_object1_bndbox_ymax.text = str(objects[i].bbox[3])
-	
-	elif i == 2:
-		# print 'add one object'
-		child_object2 = SubElement(top, 'object')
-    		child_object2_name = SubElement(child_object2, 'name')
-    		child_object2_name.text = CLASSES[objects[i].classId].lower()
- 		child_object2_pose = SubElement(child_object2, 'pose')
-		child_object2_pose.text = 'Left'
-		child_object2_truncated = SubElement(child_object2, 'truncated')
-		child_object2_truncated.text = str(objects[i].truncated)
-		child_object2_difficult = SubElement(child_object2, 'difficult')
-		child_object2_difficult.text = '0'
-
-		child_object2_bndbox = SubElement(child_object2, 'bndbox')
-		child_object2_bndbox_xmin = SubElement(child_object2_bndbox, 'xmin')
-		child_object2_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object2_bndbox_ymin = SubElement(child_object2_bndbox, 'ymin')
-		child_object2_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object2_bndbox_xmax = SubElement(child_object2_bndbox, 'xmax')
-		child_object2_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object2_bndbox_ymax = SubElement(child_object2_bndbox, 'ymax')
-		child_object2_bndbox_ymax.text = str(objects[i].bbox[3])
-	elif i == 3:
-		# print 'add one object'
-		child_object3 = SubElement(top, 'object')
-    		child_object3_name = SubElement(child_object3, 'name')
-    		child_object3_name.text = CLASSES[objects[i].classId].lower()
- 		child_object3_pose = SubElement(child_object3, 'pose')
-		child_object3_pose.text = 'Left'
-		child_object3_truncated = SubElement(child_object3, 'truncated')
-		child_object3_truncated.text = str(objects[i].truncated)
-		child_object3_difficult = SubElement(child_object3, 'difficult')
-		child_object3_difficult.text = '0'
-
-		child_object3_bndbox = SubElement(child_object3, 'bndbox')
-		child_object3_bndbox_xmin = SubElement(child_object3_bndbox, 'xmin')
-		child_object3_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object3_bndbox_ymin = SubElement(child_object3_bndbox, 'ymin')
-		child_object3_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object3_bndbox_xmax = SubElement(child_object3_bndbox, 'xmax')
-		child_object3_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object3_bndbox_ymax = SubElement(child_object3_bndbox, 'ymax')
-		child_object3_bndbox_ymax.text = str(objects[i].bbox[3])	
-	elif i == 4:
-		# print 'add one object'
-		child_object4 = SubElement(top, 'object')
-    		child_object4_name = SubElement(child_object4, 'name')
-    		child_object4_name.text = CLASSES[objects[i].classId].lower()
- 		child_object4_pose = SubElement(child_object4, 'pose')
-		child_object4_pose.text = 'Left'
-		child_object4_truncated = SubElement(child_object4, 'truncated')
-		child_object4_truncated.text = str(objects[i].truncated)
-		child_object4_difficult = SubElement(child_object4, 'difficult')
-		child_object4_difficult.text = '0'
-
-		child_object4_bndbox = SubElement(child_object4, 'bndbox')
-		child_object4_bndbox_xmin = SubElement(child_object4_bndbox, 'xmin')
-		child_object4_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object4_bndbox_ymin = SubElement(child_object4_bndbox, 'ymin')
-		child_object4_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object4_bndbox_xmax = SubElement(child_object4_bndbox, 'xmax')
-		child_object4_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object4_bndbox_ymax = SubElement(child_object4_bndbox, 'ymax')
-		child_object4_bndbox_ymax.text = str(objects[i].bbox[3])	
-	elif i == 5:
-		# print 'add one object'
-		child_object5 = SubElement(top, 'object')
-    		child_object5_name = SubElement(child_object5, 'name')
-    		child_object5_name.text = CLASSES[objects[i].classId].lower()
- 		child_object5_pose = SubElement(child_object5, 'pose')
-		child_object5_pose.text = 'Left'
-		child_object5_truncated = SubElement(child_object5, 'truncated')
-		child_object5_truncated.text = str(objects[i].truncated)
-		child_object5_difficult = SubElement(child_object5, 'difficult')
-		child_object5_difficult.text = '0'
-
-		child_object5_bndbox = SubElement(child_object5, 'bndbox')
-		child_object5_bndbox_xmin = SubElement(child_object5_bndbox, 'xmin')
-		child_object5_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object5_bndbox_ymin = SubElement(child_object5_bndbox, 'ymin')
-		child_object5_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object5_bndbox_xmax = SubElement(child_object5_bndbox, 'xmax')
-		child_object5_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object5_bndbox_ymax = SubElement(child_object5_bndbox, 'ymax')
-		child_object5_bndbox_ymax.text = str(objects[i].bbox[3])	
-	elif i == 6:
-		# print 'add one object'
-		child_object6 = SubElement(top, 'object')
-    		child_object6_name = SubElement(child_object6, 'name')
-    		child_object6_name.text = CLASSES[objects[i].classId].lower()
- 		child_object6_pose = SubElement(child_object6, 'pose')
-		child_object6_pose.text = 'Left'
-		child_object6_truncated = SubElement(child_object6, 'truncated')
-		child_object6_truncated.text = str(objects[i].truncated)
-		child_object6_difficult = SubElement(child_object6, 'difficult')
-		child_object6_difficult.text = '0'
-
-		child_object6_bndbox = SubElement(child_object6, 'bndbox')
-		child_object6_bndbox_xmin = SubElement(child_object6_bndbox, 'xmin')
-		child_object6_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object6_bndbox_ymin = SubElement(child_object6_bndbox, 'ymin')
-		child_object6_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object6_bndbox_xmax = SubElement(child_object6_bndbox, 'xmax')
-		child_object6_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object6_bndbox_ymax = SubElement(child_object6_bndbox, 'ymax')
-		child_object6_bndbox_ymax.text = str(objects[i].bbox[3])
-
-	elif i == 7:
-		# print 'add one object'
-		child_object7 = SubElement(top, 'object')
-    		child_object7_name = SubElement(child_object7, 'name')
-    		child_object7_name.text = CLASSES[objects[i].classId].lower()
- 		child_object7_pose = SubElement(child_object7, 'pose')
-		child_object7_pose.text = 'Left'
-		child_object7_truncated = SubElement(child_object7, 'truncated')
-		child_object7_truncated.text = str(objects[i].truncated)
-		child_object7_difficult = SubElement(child_object7, 'difficult')
-		child_object7_difficult.text = '0'
-
-		child_object7_bndbox = SubElement(child_object7, 'bndbox')
-		child_object7_bndbox_xmin = SubElement(child_object7_bndbox, 'xmin')
-		child_object7_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object7_bndbox_ymin = SubElement(child_object7_bndbox, 'ymin')
-		child_object7_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object7_bndbox_xmax = SubElement(child_object7_bndbox, 'xmax')
-		child_object7_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object7_bndbox_ymax = SubElement(child_object7_bndbox, 'ymax')
-		child_object7_bndbox_ymax.text = str(objects[i].bbox[3])
-
-	elif i == 8:
-		# print 'add one object'
-		child_object8 = SubElement(top, 'object')
-    		child_object8_name = SubElement(child_object8, 'name')
-    		child_object8_name.text = CLASSES[objects[i].classId].lower()
- 		child_object8_pose = SubElement(child_object8, 'pose')
-		child_object8_pose.text = 'Left'
-		child_object8_truncated = SubElement(child_object8, 'truncated')
-		child_object8_truncated.text = str(objects[i].truncated)
-		child_object8_difficult = SubElement(child_object8, 'difficult')
-		child_object8_difficult.text = '0'
-
-		child_object8_bndbox = SubElement(child_object8, 'bndbox')
-		child_object8_bndbox_xmin = SubElement(child_object8_bndbox, 'xmin')
-		child_object8_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object8_bndbox_ymin = SubElement(child_object8_bndbox, 'ymin')
-		child_object8_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object8_bndbox_xmax = SubElement(child_object8_bndbox, 'xmax')
-		child_object8_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object8_bndbox_ymax = SubElement(child_object8_bndbox, 'ymax')
-		child_object8_bndbox_ymax.text = str(objects[i].bbox[3])
-
-	elif i == 9:
-		# print 'add one object'
-		child_object9 = SubElement(top, 'object')
-    		child_object9_name = SubElement(child_object9, 'name')
-    		child_object9_name.text = CLASSES[objects[i].classId].lower()
- 		child_object9_pose = SubElement(child_object9, 'pose')
-		child_object9_pose.text = 'Left'
-		child_object9_truncated = SubElement(child_object9, 'truncated')
-		child_object9_truncated.text = str(objects[i].truncated)
-		child_object9_difficult = SubElement(child_object9, 'difficult')
-		child_object9_difficult.text = '0'
-
-		child_object9_bndbox = SubElement(child_object9, 'bndbox')
-		child_object9_bndbox_xmin = SubElement(child_object9_bndbox, 'xmin')
-		child_object9_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object9_bndbox_ymin = SubElement(child_object9_bndbox, 'ymin')
-		child_object9_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object9_bndbox_xmax = SubElement(child_object9_bndbox, 'xmax')
-		child_object9_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object9_bndbox_ymax = SubElement(child_object9_bndbox, 'ymax')
-		child_object9_bndbox_ymax.text = str(objects[i].bbox[3])
-
-	elif i == 10:
-		# print 'add one object'
-		child_object10 = SubElement(top, 'object')
-    		child_object10_name = SubElement(child_object10, 'name')
-    		child_object10_name.text = CLASSES[objects[i].classId].lower()
- 		child_object10_pose = SubElement(child_object10, 'pose')
-		child_object10_pose.text = 'Left'
-		child_object10_truncated = SubElement(child_object10, 'truncated')
-		child_object10_truncated.text = str(objects[i].truncated)
-		child_object10_difficult = SubElement(child_object10, 'difficult')
-		child_object10_difficult.text = '0'
-
-		child_object10_bndbox = SubElement(child_object10, 'bndbox')
-		child_object10_bndbox_xmin = SubElement(child_object10_bndbox, 'xmin')
-		child_object10_bndbox_xmin.text = str(objects[i].bbox[0])
-		child_object10_bndbox_ymin = SubElement(child_object10_bndbox, 'ymin')
-		child_object10_bndbox_ymin.text = str(objects[i].bbox[1])
-		child_object10_bndbox_xmax = SubElement(child_object10_bndbox, 'xmax')
-		child_object10_bndbox_xmax.text = str(objects[i].bbox[2])
-		child_object10_bndbox_ymax = SubElement(child_object10_bndbox, 'ymax')
-		child_object10_bndbox_ymax.text = str(objects[i].bbox[3])
+        child_object_bndbox[i] = SubElement(child_object[i], 'bndbox')
+        child_object_bndbox_xmin[i] = SubElement(child_object_bndbox[i], 'xmin')
+        child_object_bndbox_xmin[i].text = str(objects[i].bbox[0])
+        child_object_bndbox_ymin[i] = SubElement(child_object_bndbox[i], 'ymin')
+        child_object_bndbox_ymin[i].text = str(objects[i].bbox[1])
+        child_object_bndbox_xmax[i] = SubElement(child_object_bndbox[i], 'xmax')
+        child_object_bndbox_xmax[i].text = str(objects[i].bbox[2])
+        child_object_bndbox_ymax[i] = SubElement(child_object_bndbox[i], 'ymax')
+        child_object_bndbox_ymax[i].text = str(objects[i].bbox[3])
     
     tree = ET.ElementTree(top)
     tree.write(outname)
